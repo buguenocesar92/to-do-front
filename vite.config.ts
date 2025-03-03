0a1795fb0900d96718d3f8473d452c8c87c655a4
buguenocesar92 <buguenocesar92@gmail.com>
2025-03-03 15:43:02 -0300

    fix icons

--- vite.config.ts
+++ vite.config.ts
@@ -1,16 +1,24 @@
 import { fileURLToPath, URL } from 'node:url'
-
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueJsx from '@vitejs/plugin-vue-jsx'
 import vueDevTools from 'vite-plugin-vue-devtools'
+import path from 'node:path'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [vue(), vueJsx(), vueDevTools()],
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url)),
-    },
+      '@fortawesome': path.resolve(__dirname, 'node_modules/@fortawesome')
+    }
   },
+  server: {
+    fs: {
+      allow: [
+        path.resolve(__dirname, './'),
+        'C:/Users/admin/dev/to-do-app/to-do-front/node_modules/.pnpm'
+      ]
+    }
+  }
 })
